7cd3d2388cf77989ba6c6c6620cb3a9efc9c4fb0
labiybafakh <labiybafakh@gmail.com>
2025-06-28 03:01:45 +0900

    Fix issue on intensity

--- src/rs_to_velodyne.cpp
+++ src/rs_to_velodyne.cpp
@@ -28,14 +28,14 @@ static int RING_ID_MAP_16[] = {
 // rslidar的点云格式
 struct RsPointXYZIRT {
     PCL_ADD_POINT4D;
-    uint8_t intensity;
-    uint16_t ring = 0;
-    double timestamp = 0;
+    PCL_ADD_INTENSITY;
+    uint16_t ring;
+    double timestamp;
 
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 } EIGEN_ALIGN16;
 POINT_CLOUD_REGISTER_POINT_STRUCT(RsPointXYZIRT,
-                                  (float, x, x)(float, y, y)(float, z, z)(uint8_t, intensity, intensity)
+                                  (float, x, x)(float, y, y)(float, z, z)(float, intensity, intensity)
                                           (uint16_t, ring, ring)(double, timestamp, timestamp))
 
 // velodyne的点云格式
